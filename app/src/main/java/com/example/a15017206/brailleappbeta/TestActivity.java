f01d23691298d71aaa8829a49abdb66dacb988b9
15017206 <15017206@myrp.edu.sg>
2017-11-29 11:12:44 +0900

    1

--- app/src/main/java/com/example/a15017206/brailleappbeta/TestActivity.java
+++ app/src/main/java/com/example/a15017206/brailleappbeta/TestActivity.java
@@ -2,12 +2,12 @@ package com.example.a15017206.brailleappbeta;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.regex.Pattern;
@@ -15,11 +15,13 @@ import java.util.regex.Pattern;
 public class TestActivity extends AppCompatActivity {
 
     String TAG = ">/>";
-    ArrayList<String> arraylist_output2 = new ArrayList<>();
+
     TextView tvOutput;
     EditText etInput;
     Button btnDoTranslate;
     String output = "";
+    ArrayList<String> arraylist_output2;
+    Boolean numericMode = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,59 +34,89 @@ public class TestActivity extends AppCompatActivity {
 
         final doTranslateEtoB obj1 = new doTranslateEtoB();
 
-        etInput.setText("i");
-
-        output = "";
-        arraylist_output2 = obj1.separateToIndividualArraysWord(etInput.getText().toString());
-        output += arraylist_output2 + "\n";
-        arraylist_output2 = obj1.detectNumbers(arraylist_output2);
-        output += arraylist_output2 + "\n";
-        arraylist_output2 = obj1.detectCapitalisation(arraylist_output2);
-        output += arraylist_output2 + "\n";
-        arraylist_output2 = obj1.separateToIndividualArraysLetter(arraylist_output2);
-        output += arraylist_output2 + "\n";
-        arraylist_output2 = obj1.convertLettersAndNumbersToBinary(arraylist_output2);
-        output += arraylist_output2 + "\n";
-        arraylist_output2 = obj1.convertBinaryToDecimals(arraylist_output2);
-        output += arraylist_output2 + "\n";
-        tvOutput.setText(output);
-
+//        etInput.setText("Prof Ito");
 
         btnDoTranslate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                output = "";
-//                arraylist_output2 = obj1.separateToIndividualArraysWord(etInput.getText().toString());
-//
-//                arraylist_output2 = obj1.detectNumbers(arraylist_output2);
-//                arraylist_output2 = obj1.detectCapitalisation(arraylist_output2);
-//                arraylist_output2 = obj1.separateToIndividualArraysLetter(arraylist_output2);
-//                Log.i(TAG, "Indiv letters/numbers: " + arraylist_output2);
-//                output += arraylist_output2 + "\n";
-//
-//                arraylist_output2 = obj1.convertLettersAndNumbersToBinary(arraylist_output2);
-//                Log.i(TAG, "In binary form is: " + arraylist_output2);
-//                output += arraylist_output2 + "\n";
-//
-//                arraylist_output2 = obj1.convertBinaryToDecimals(arraylist_output2);
-//                Log.i(TAG, "In decimal form is: " + arraylist_output2);
-//                output += arraylist_output2 + "\n";
-//
-//                tvOutput.setText(output);
+                output = "";
+                arraylist_output2 = obj1.separateToIndividualArraysWord(etInput.getText().toString());
+                output += arraylist_output2 + "\n\n";
+                arraylist_output2 = obj1.detectNumbers(arraylist_output2);
+                output += arraylist_output2 + "\n\n";
+                arraylist_output2 = obj1.detectCapitalisation(arraylist_output2);
+                output += arraylist_output2 + "\n\n";
+                arraylist_output2 = obj1.separateToIndividualArraysLetter(arraylist_output2);
+                output += arraylist_output2 + "\n\n";
+                arraylist_output2 = obj1.convertLettersAndNumbersToBinary(arraylist_output2);
+                output += arraylist_output2 + "\n\n";
+                arraylist_output2 = obj1.convertBinaryToDecimals(arraylist_output2);
+                output += arraylist_output2 + "\n\n";
+                tvOutput.setText(output);
             }
         });
+//        btnDoTranslate.performClick();
     }
 
     class doTranslateEtoB {
 
-        public ArrayList separateToIndividualArraysWord(String input_string) {
+        public ArrayList separateToIndividualArraysWord(String input_string0) {
 
             // 1a. Using a .split(" ")-> ["THIS" "IS" "A" "PASSAGE." "I" "LOVE" "eating" "3" "ice-creams." "Don't" "you," "Tom?"]
-            ArrayList<String> arraylist_output = new ArrayList<String>(Arrays.asList(input_string.split(" ")));
+//            ArrayList<String> arraylist_output = new ArrayList<String>(Arrays.asList(input_string0.split(" ")));
 
-            for (int i = 0; i < arraylist_output.size(); i++){
-                arraylist_output.add(" ");
+            String[] temp_array = input_string0.split(" ");
+            Log.i(TAG, "separateToIndividualArraysWord: " + temp_array);
+            ArrayList<String> arraylist_output = new ArrayList<String>();
+
+            int counter_arraylist = 0;
+            for (int i = 0; i < temp_array.length; i++) {
+
+//                if (!Pattern.matches("\\b[A-Za-z]+[\\d]+\\b|\\b[\\d]+[A-Za-z]+\\b", temp_array[i] + "")) {
+                arraylist_output.add(counter_arraylist, temp_array[i]);
+
+                // if the index is the last, dont add additional " " <- empty string
+                if (i + 1 < temp_array.length) {
+                    arraylist_output.add(counter_arraylist + 1, " ");
+                }
+                counter_arraylist = arraylist_output.size();
+//                }
             }
+
+
+            for (int i = 0; i < arraylist_output.size(); i++) {
+                if ((arraylist_output.get(i) + "").matches("\\b\\d+[A-Za-z]+|[A-Za-z]+\\d+\\b")) {
+                    ArrayList<String> temp_arraylist = new ArrayList<String>(Arrays.asList((arraylist_output.get(i) + "").split("")));
+                    Log.i(TAG, "separateToIndividualArraysWord: " + temp_arraylist);
+                    arraylist_output.remove(i);
+                    temp_arraylist.remove(0);
+                    arraylist_output.addAll(i, temp_arraylist);
+
+                }
+            }
+
+            for (int i = 0; i < arraylist_output.size(); i++) {
+                if (i + 1 < arraylist_output.size()) {
+                    if (Pattern.matches("\\b[A-Za-z]+[\\d]+\\b|\\b[\\d]+[A-Za-z]+\\b", arraylist_output.get(i) + "")) {
+                        Log.i(TAG, "separateToIndividualArraysWord: " + arraylist_output);
+                    }
+                }
+            }
+
+            // to check for letters & numbers together, eg. i7, T34,
+            for (int i = 0; i < arraylist_output.size(); i++) {
+                // separate letters and words together, 32p, i7, u30s
+                if (Pattern.matches("\\b[A-Za-z]+[\\d]+\\b|\\b[\\d]+[A-Za-z]+\\b", arraylist_output.get(i) + "")) {
+                    String y = "" + arraylist_output.get(i);
+                    ArrayList<String> temporary_arraylist2 = new ArrayList<String>(Arrays.asList(y.split("")));
+                    temporary_arraylist2.remove(0);
+                    arraylist_output.addAll(i, temporary_arraylist2);
+                    arraylist_output.remove(i + temporary_arraylist2.size());
+                    i += temporary_arraylist2.size();
+
+                }
+            }
+
 
             // for "-"
             for (int i = 0; i < arraylist_output.size(); i++) {
@@ -133,31 +165,41 @@ public class TestActivity extends AppCompatActivity {
                     }
                 }
             }
+
+
             return arraylist_output;
             // return Integer.parseInt(c, 2);
         }
 
         public ArrayList detectNumbers(ArrayList input_arraylist) {
+            numericMode = false;
             for (int i = 0; i < input_arraylist.size(); i++) {
                 String x = input_arraylist.get(i) + "";
-
-                // If string x is a number
+                // If string x is a number P.S. this can allow a 77 or 7.7
                 if (x.matches("\\d+")) {
+
                     ArrayList<String> temp_arraylist = new ArrayList<>(Arrays.asList(x.split("")));
                     temp_arraylist.remove(0);
                     input_arraylist.remove(i);
 
                     input_arraylist.addAll(i, temp_arraylist);
-                    input_arraylist.add(i, "001111"); //this is numeric indicator
-                    i = i + temp_arraylist.size();
 
-//                    if (i < input_arraylist.size()) { // If a digit is the last index of an arraylist
-//                        if ((input_arraylist.get(i + 1) + "").matches("\\b[A-Za-z]+\\b")) {
-//                            //To add a grade 1 indicator if there is letters behind numbers
-//                            String z = input_arraylist.get(i + 1) + "";
-//                            input_arraylist.add(i + 1, "000011");
-//                            i++;
-//                        }
+                    if (numericMode == false) {
+                        input_arraylist.add(i, "001111");
+                        numericMode = true;
+                    }
+
+                    i = i + temp_arraylist.size();
+                }
+
+                // is i NOT the last index? Need to know, as we need to add a grade 1 indicator if a word is behind a numeral
+                if (i + 1 < input_arraylist.size()) {
+
+                    // is there a " " or " ", "<<sth here>>" after index i? eg. 22 pens, 90 ink - need to put grade 1 indicator
+//                    if ((input_arraylist.get(i) + "").matches(" ") && (input_arraylist.get(i + 1) + "").matches("\\b[A-Ja-j]+[A-Za-z]*\\b") || (input_arraylist.get(i) + "").matches("\\b[A-Ja-j]+[A-Za-z]*\\b")) {
+//                        input_arraylist.add(i + 1, "000011");
+//                        numericMode = false;
+//                        i += 2;
 //                    }
                 }
             }
@@ -169,7 +211,6 @@ public class TestActivity extends AppCompatActivity {
             boolean detect_capital_word = false;
 
             for (int i = 0; i < input_arraylist.size(); i++) {
-
                 //detects for capital letters in front of a word
                 if (detect_capital_letter = Pattern.matches("\\b[A-Z]{1}[a-z]{1,}\\b", "" + input_arraylist.get(i))) {
                     input_arraylist.add(i, "000001");
@@ -186,22 +227,20 @@ public class TestActivity extends AppCompatActivity {
                     temp_arraylist.add("000001");
                     input_arraylist.addAll(j, temp_arraylist);
                     j = j + 2;
-//                    Log.i(TAG, "detectCapitalisation: j is: " + j + " & the word is: " + input_arraylist.get(j));
                 }
             }
-
-            //Log.i(TAG, "detectCapitalisation: ended:" + input_arraylist);
             return input_arraylist;
         }
 
         private ArrayList separateToIndividualArraysLetter(ArrayList input_arraylist2) {
 
-            // Log.i(TAG, "separateToIndividualArraysLetter: " + input_arraylist2);
             for (int i = 0; i < input_arraylist2.size(); i++) {
+                // need to separate
+                if (Pattern.matches("\\b[A-Z |a-z]{2,}\\b", "" + input_arraylist2.get(i))) {
+                }
 
-                // check if the index contains a legit word
-                if (Pattern.matches("\\b[A-Z | a-z]{2,}\\b", "" + input_arraylist2.get(i))) {
-
+                // check if the index contains a legit word, "phone", "cow", "mouse", etc
+                if (Pattern.matches("\\b[A-Z |a-z]{2,}\\b", "" + input_arraylist2.get(i))) {
                     // This is assumed to be in grade 1 Braille
                     String x = "" + input_arraylist2.get(i);
                     ArrayList<String> temporary_arraylist = new ArrayList<String>(Arrays.asList(x.split("")));
@@ -211,6 +250,31 @@ public class TestActivity extends AppCompatActivity {
                     // i = i + temporary_arraylist.size();
                 }
 
+                //STEP 3: when words like, iPhone, macPherson, when a uppercase letter in a lowercase word
+                // Need index i(lowercase letter), and i+1(uppercase letter)
+                // if the arraylist size is at least 2, and 1st index is a lowercase, and 2nd index is uppercase
+                if (input_arraylist2.size() >= 2 && i < input_arraylist2.size()-1) {
+                    if (Pattern.matches("\\b[a-z]+\\b", "" + input_arraylist2.get(i)) && Pattern.matches("\\b[A-Z]+\\b", "" + input_arraylist2.get(i + 1))){
+                        input_arraylist2.add(i+1, "000001");
+                    }
+                }
+
+//                String y = "" + input_arraylist2.get(i);
+//                ArrayList<String> temporary_arraylist2 = new ArrayList<String>(Arrays.asList(y.split("")));
+//                temporary_arraylist2.remove("");
+//                Log.i(TAG, "separateToIndividualArraysLetter: " + temporary_arraylist2);
+
+//                // separate letters and words together, 32p, i7, u30s
+//                if (Pattern.matches("\\b[A-Za-z]+[\\d]+\\b|\\b[\\d]+[A-Za-z]+\\b", input_arraylist2.get(i)+"")){
+//                    String y = "" + input_arraylist2.get(i);
+//                    ArrayList<String> temporary_arraylist2 = new ArrayList<String>(Arrays.asList(y.split("")));
+//                    temporary_arraylist2.remove(0);
+//                    input_arraylist2.addAll(i, temporary_arraylist2);
+//                    input_arraylist2.remove(i+temporary_arraylist2.size());
+//                    i+= temporary_arraylist2.size();
+//
+//                }
+
             }
             return input_arraylist2;
         }
@@ -219,7 +283,9 @@ public class TestActivity extends AppCompatActivity {
             ArrayList<String> temp_arraylist = new ArrayList<>();
             for (int i = 0; i < input_arraylist3.size(); i++) {
                 String x = input_arraylist3.get(i) + "";
-                if (x.matches("\\b[A-Za-z0-9]\\b|,")) {
+
+                //Some binary code is already added. Need this if else to filter out these existing binary codes, else the library switch case wont recognise it.
+                if (x.matches("\\b[A-Za-z0-9]\\b| ")) {
                     temp_arraylist.add("" + wordToBinary(x));
                 } else {
                     temp_arraylist.add(x);
@@ -244,6 +310,8 @@ public class TestActivity extends AppCompatActivity {
         //These 2 are libraries created by me
         public String wordToBinary(String x) {
             switch (x.toLowerCase()) {
+                case " ":
+                    return "000000";
                 case "a":
                 case "1":
 //                    return Integer.parseInt("100000", 2); //32
@@ -347,6 +415,8 @@ public class TestActivity extends AppCompatActivity {
 
         public int binaryToDecimal(String y) {
             switch (y) {
+                case "000000":
+                    return Integer.parseInt("000000", 2); // 0
                 case "100000": // a/1
                     return Integer.parseInt("100000", 2); //32;
                 case "110000": // b/2
